c66518fda6e67ab2605b5b690ca519891ec7b8d3
xingweiyong <1531694185@qq.com>
2017-12-12 09:29:04 +0800

    Update settings.py

--- Devops/settings.py
+++ Devops/settings.py
@@ -1,9 +1,9 @@
 # coding:utf-8
 
 # 数据库 <-> 责任人邮箱
-db_map = {'leader':['zhangmingjin@jd.com'],
-          'zb_yy':['xingweiyong@jd.com'],
-          'zb_momo':['xingweiyong@jd.com'],
-          'guba':['xingweiyong@jd.com'],
-          'xueqiu':['xingweiyong@jd.com'],
-          'tieba':['xingweiyong@jd.com']}
+db_map = {'leader':['***@**.com'],
+          'zb_yy':['***@**.com'],
+          'zb_momo':['***@**.com'],
+          'guba':['***@**.com'],
+          'xueqiu':['***@**.com'],
+          'tieba':['***@**.com']}
